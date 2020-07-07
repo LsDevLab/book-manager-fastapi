cc8ec884e8626d2a3418362b47e64692f522e270
Luigi Schiavone <luigisc98@gmail.com>
2020-07-07 20:04:08 +0200

    no message

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import fastapi as fastAPI
+from fastapi import FastAPI
+import json
+
+api = FastAPI()
+
+books = json.load(open('books.json'))
+
+@api.get('/manager')
+async def root():
+    return {"api-name": "book-manager", "version": 1.0}
+
+@api.get('/manager/books')
+async def get_books():
+    return books
